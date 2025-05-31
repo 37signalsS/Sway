ecd22af8f6bfccffe81e00e28b562e38c4a23895
37signalsS <misha-eliseyev@mail.ru>
2025-05-31 21:26:30 +0300

    docs: hh

--- waybar/scripts/screenshots.sh
+++ waybar/scripts/screenshots.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 SAVE_DIR="/home/q/Изображения/screenshots"
+
 mkdir -p "$SAVE_DIR"
 
 TMP_FILE="/tmp/screenshot.png"
-FINAL_FILE="${SAVE_DIR}/$(date +'%Y-%m-%d_%H-%M-%S').png"
+
+FINAL_FILE="${SAVE_DIR}/$(date +'%H:%M %d.%m.%Y').png"
 
 grim -g "$(slurp)" "$TMP_FILE" && swappy -f "$TMP_FILE" -o "$FINAL_FILE"
+
+[[ -f "$TMP_FILE" ]] && rm "$TMP_FILE"
