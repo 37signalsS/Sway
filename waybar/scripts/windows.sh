58ba10b9bed909d353f309e8f69f83b2c960daa2
37signalsS <misha-eliseyev@mail.ru>
2025-06-12 15:51:22 +0300

    add

--- waybar/scripts/windows.sh
+++ waybar/scripts/windows.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+# Получаем список всех окон в формате JSON с помощью swaymsg
 window_list=$(swaymsg -t get_tree | jq -r '.. | select(.type?) | select(.type == "con" or .type == "floating_con") | select(.app_id != null or .window_properties.class != null) | "\(.id)|\(.app_id // .window_properties.class)|\(.name)"')
 
+# Проверяем, есть ли окна
 if [ -z "$window_list" ]; then
-    notify-send "Error 1"
+    notify-send "Ошибка" "Нет доступных окон для выбора"
     exit 1
 fi
 
+# Преобразуем список окон в удобный для wofi формат
 formatted_list=""
 while IFS= read -r line; do
     window_id=$(echo "$line" | cut -d '|' -f 1)
@@ -15,17 +18,21 @@ while IFS= read -r line; do
     formatted_list+="$app_name - $window_title\n"
 done <<< "$window_list"
 
-selected=$(echo -e "$formatted_list" | wofi --dmenu --prompt="Select window")
+# Показываем список окон через wofi и получаем выбранный элемент
+selected=$(echo -e "$formatted_list" | wofi --dmenu --prompt="Выберите окно")
 
+# Если пользователь выбрал что-то
 if [ -n "$selected" ]; then
     # Извлекаем ID выбранного окна
     selected_app=$(echo "$selected" | awk '{print $1}')
     selected_id=$(echo "$window_list" | grep "|$selected_app|" | head -n 1 | cut -d '|' -f 1)
     
+    # Проверяем, что ID найден
     if [ -z "$selected_id" ]; then
-        notify-send "Error 2"
+        notify-send "Ошибка" "Не удалось найти ID окна"
         exit 1
     fi
 
+    # Активируем выбранное окно
     swaymsg "[con_id=$selected_id]" focus
 fi
--- wofi/power.sh
+++ wofi/power.sh
@@ -2,13 +2,15 @@
 
 entries="⇠ Logout\n⏾ Suspend\n󰜉 Reboot\n⏻ Shutdown"
 
-selected=$(echo -e $entries|wofi -c ~/.config/wofi/power -s ~/.config/wofi/power.css  -x 1670 -y 10 --height 150 --width 80 --dmenu --cache-file /dev/null --hide-scroll | awk '{print tolower($2)}')
+selected=$(echo -e $entries|wofi -c ~/.config/wofi/power -s ~/.config/wofi/power.css  -x 2170 -y 10 --height 150 --width 80 --dmenu --cache-file /dev/null --hide-scroll | awk '{print tolower($2)}')
 
 case $selected in
   logout)
     swaymsg exit;;
   suspend)
-    exec systemctl suspend;;
+    exec swaylock -f -C ~/.config/swaylock/config &
+	sleep 1
+    systemctl suspend;;
   reboot)
     exec systemctl reboot;;
   shutdown)
